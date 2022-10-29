065911714e6852591478dfca0c5fc000c89a6271
Subi-Babu <subiaby4613@gmail.com>
2022-10-29 22:17:45 +0530

    commit

--- lib/home.dart
+++ lib/home.dart
@@ -1,87 +1,110 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'mainscreen.dart';
-
 void main(){
-  runApp(MaterialApp(home: HomePage(),));
+  runApp(MaterialApp(home: MainScreen(),));
 }
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  bool viewenable = true;
-
+class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    Navigator.pop(context);
-
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("LoginPage",),
-          //centerTitle: true,
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Text(
-                  "LoginPage",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(15.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon:  Icon(Icons.account_circle),
-                      label: Text("UserName"),
-                      hintText: "UserName must be your email",
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(80)),
-                      )),
-                ),
-              ),
-               Padding(
-                padding: EdgeInsets.all(15.0),
-                child: TextField(
-                 // obscuringCharacter: "*",
-                  obscureText: viewenable , // true
-                  decoration:  InputDecoration(
-                      suffixIcon: IconButton(
-                          onPressed: (){
-                             setState(() {
-                               if(viewenable){
-                                 viewenable = false;
-                               }else{
-                                 viewenable = true;
-                               }
-                             });
-                      }, icon: Icon(viewenable == true? Icons.visibility_off : Icons.visibility)),
 
-                      prefixIcon: const Icon(Icons.password) ,
-                      label: const Text("Password"),
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      )),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 20, bottom: 20),
-                child: ElevatedButton(
-                    onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MainScreen()));
-                },
-                    child: const Text("LOGIN")),
-              ),
-              TextButton(
-                  onPressed: () { },
-                  child: const Text("Not a User? Register Here!!!1"))
-            ],
+      appBar: AppBar(
+        title: const Text("ListView"),
+      ),
+      body: ListView(
+        children:  [
+          const SizedBox(
+            height: 10,
           ),
-        ),
-        );
+          const Center(
+              child: Text(
+            "MY CONTACTS",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          )),
+          const SizedBox(
+            height: 10,
+          ),
+          Card(
+            child: ListTile(
+              title: const Text("ANU"),
+              //tileColor: Colors.grey,
+              subtitle: const Text("9685742036"),
+              trailing: Wrap(
+                children: const [
+                  Icon(Icons.sms_sharp,color: Colors.blue),
+                  SizedBox(width: 30,),
+                  Icon(Icons.phone,color: Colors.blue),
+                ],
+              ),
+              leading: const CircleAvatar(
+                radius:50,
+                backgroundImage: NetworkImage(
+                    "https://www.shutterstock.com/image-photo/close-portrait-casually-dressed-handsome-600w-627946895.jpg"),
+              ),
+              //onTap: ,
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: const Text("ANU"),
+              //tileColor: Colors.grey,
+              subtitle: const Text("9685742036"),
+              trailing: Wrap(
+                children: const [
+                  Icon(Icons.sms_sharp,color: Colors.blue),
+                  SizedBox(width: 30,),
+                  Icon(Icons.phone,color: Colors.blue),
+                ],
+              ),
+              leading: const CircleAvatar(
+                radius:50,
+                backgroundImage: NetworkImage(
+                    "https://www.shutterstock.com/image-photo/close-portrait-casually-dressed-handsome-600w-627946895.jpg"),
+              ),
+              //onTap: ,
+            ),
+          ),
+           const Card(
+             child: ListTile(
+              title: Text("JOHN"),
+              //tileColor: Colors.grey,
+              subtitle: Text("8954712563"),
+              trailing: Icon(Icons.phone,color: Colors.blue),
+              leading: CircleAvatar(
+                radius:50,
+                backgroundImage: NetworkImage(
+                    "https://image.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg"),
+              ),
+              //onTap: ,
+          ),
+           ),
+           const Card(
+             child: ListTile(
+              title: Text("JO"),
+              subtitle: Text("89789512563"),
+              trailing: Icon(Icons.phone,color: Colors.blue),
+              leading: CircleAvatar(
+                radius:50,
+                backgroundImage: NetworkImage(
+                    "https://image.shutterstock.com/image-photo/head-shot-portrait-close-smiling-260nw-1714666150.jpg"),
+              ),
+              //onTap: ,
+          ),
+           ),
+         const Card(
+            child:  ListTile(
+              title: Text("CHINNU"),
+                subtitle: Text("9685741235"),
+              trailing: Icon(Icons.phone,color: Colors.blue),
+              leading: CircleAvatar(
+                radius:50,
+                backgroundImage: NetworkImage(
+                    "https://www.shutterstock.com/image-photo/happy-asian-little-child-girl-600w-644737381.jpg"),
+              ),
+                          ),
+          ),
+        ],
+      ),
+    );
   }
 }
