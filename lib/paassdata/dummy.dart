936f724b5f4bd645b4974536d2b856a0a0ce37ba
Subi-Babu <subiaby4613@gmail.com>
2022-11-12 23:39:03 +0530

    commit

---
+++ lib/paassdata/dummy.dart
@@ -0,0 +1,30 @@
+const List<Map<String, dynamic>> dummyProducts = [
+  {
+    "id": 1,
+    "name": "PRODUCT1",
+    "price": 1000,
+    "description": "This is the description of Product 1"
+    //"image"   : ""
+  },
+  {
+    "id": 2,
+    "name": "PRODUCT2",
+    "price": 2000,
+    "description": "This is the description of Product 2"
+   // "image"  :
+  },
+  {
+    "id": 3,
+    "name": "PRODUCT3",
+    "price": 3000,
+    "description": "This is the description of Product 3"
+    // "image"  :
+  },
+  {
+    "id": 4,
+    "name": "Product4",
+    "price": 4000,
+    "description": "This is the description of Product 4"
+    // "image"  :
+  },
+];
